71e8f26e2b161a0087bca4e6979e1597a4aaf8f8
unknown <tronghung122005@gmail.com>
2025-04-23 10:39:44 +0700

    New

---
+++ Lesson01MMC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lesson01MMC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
